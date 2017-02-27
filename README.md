e9ba4ccf899aa358d98625412ef92e4ae8c71870
toplan <toplan710@gmail.com>
2017-02-27 23:31:48 +0800

    upgrade

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Intro
-A library to make your enqueue function lazily.
+A lightweight and no dependencies library to make your enqueue function lazily.
 
 # Install
 ```
@@ -18,17 +18,17 @@ const lazilyEnqueue = lazy(enqueue, {
     delay: 1000,
     limit: 3,
     dequeue: dequeue,
-    will: value => {
+    will: data => {
         console.group()
         console.timeEnd('time')
-        console.log('will enqueue:', value)
+        console.log('[will] data:', data)
     },
-    rejected: value => {
-        console.warn('rejected enqueue:', value)
-    },
-    did: value => {
-        console.log('queue:', queue)
+    success: (value, data) => {
+        console.log('[success] queue:', queue)
         console.groupEnd()
+    },
+    failure: (err, data) => {
+        console.warn('[failure] data:', data, ', and the reason is', err)
     }
 })
 
@@ -41,39 +41,68 @@ setTimeout(() => {
 
 # API
 
+## lazy(enqueue, [options])
 ```javascript
 import lazy from 'lazy-enqueue'
 ```
-
-## lazy([options], enqueue)
-
 Create a lazily high order function of the original enqueue function.
 
 #### Arguments
-1. `[options]` (Object)
+1. `enqueue` (Function)
 
-- delay (Number|Promise|Function)
-  The value can be a number, promise or function witch return a number or promise, default value is `0`.
-  If the value is a negative number, the data will skip delay and enqueue immediately after the previous.
+    The original enqueue function.
 
-- will (Function)
-  A global hook, will be invoke before enqueue with the arguments same to enqueue function.
+2. `[options]` (Object)
 
-- did (Function)
-  A global hook, will be invoke after enqueue with the arguments same to enqueue function.
+    - delay (Number|Promise|Function): global delay value.
 
-- rejected (Function)
-  A global hook, will be invoke after reject enqueue with the arguments same to enqueue function.
+    - will (Function): A global hook, called before enqueue.
 
-- limit (Number)
-  The number of limit enqueue, default value is `Infinity`.
+    - success (Function): A global hook, called after successful enqueue.
 
-- dequeue (Function)
-  A dequeue function of the original queue, this option is required if the limit option is not equal to `Infinity`.
+    - failure (Function): A global hook, called after the failure of enqueue.
 
-2. `enqueue` (Function)
-The original enqueue function.
+    - limit (Number): The number of limit enqueue, default value is `Infinity`.
+
+    - dequeue (Function): A dequeue function of the original queue, this option is required if the limit option is lower than `Infinity`.
 
 #### Returns
-
 `(lazilyEnqueue)`: A lazily high order function.
+
+## delay(value)
+```javascript
+const {delay} = lazilyEnqueue(data)
+```
+The method can override the global delay value.
+
+#### Arguments
+`value` (Number|Promise|Function)
+
+## hook(name, fn)
+```javascript
+const {hook} = lazilyEnqueue(data)
+```
+Add private hooks, which will invoke before the global hooks.
+
+#### Arguments
+1. `name` (String)
+
+    There are only three optional value: 'will', 'success', 'failure'.
+
+2. `fn` (Function)
+
+    The hook handler.
+    If the name is `success`, the first argument is the return value of the original enqueue function.
+    If the name is `failure`, the first argument is the error information.
+
+## will(fn)
+```javascript
+const {will} = lazilyEnqueue(data)
+```
+alias: `hook('will', fn)`
+
+## done(onSuccess, onFailure)
+```javascript
+const {done} = lazilyEnqueue(data)
+```
+alias: `hook('success', onSuccess).hook('failure', onFailure)`
--- index.js
+++ index.js
@@ -18,6 +18,70 @@
   }
 }(this, function (utils) {
   return function () { //factory
+    var OVERFLOW_ERROR = new Error('overflow from the buffer')
+    //status
+    var PENDING = 'PENDING', SUCCESS = 'SUCCESS', FAILED = 'FAILED'
+    //hooks
+    var WILL_HOOK = 'will', SUCCESS_HOOK = 'success', FAILURE_HOOK = 'failure'
+
+    var success = function (data, value) {
+      data.value = value
+      data.status = SUCCESS
+      try {
+        utils.invoke(data.successHooks, [value].concat(data.args))
+      } catch (e) {
+        utils.warn(e)
+      }
+    }
+
+    var failure = function (data, error) {
+      data.error = error
+      data.status = FAILED
+      try {
+        utils.invoke(data.failureHooks, [error].concat(data.args))
+      } catch (e) {
+        utils.warn(e)
+      }
+    }
+
+    var createLazyReturns = function (data) {
+      var returns
+
+      return returns = {
+        delay: function(delay) {
+          data.delay = delay
+          return returns
+        },
+        hook: function(name, fn) {
+          if (typeof fn !== 'function') {
+            return returns
+          }
+          if (data.status !== PENDING && name === WILL_HOOK) {
+            return returns
+          }
+          if (data.status === SUCCESS && name === SUCCESS_HOOK) {
+            utils.invoke(fn, [data.value].concat(data.args))
+            return returns
+          }
+          if (data.status === FAILED && name === FAILURE_HOOK) {
+            utils.invoke(fn, [data.error].concat(data.args))
+            return returns
+          }
+          name = name + 'Hooks'
+          if (name in data) {
+            data[name].push(fn)
+          }
+          return returns
+        },
+        will: function(fn) {
+          return returns.hook(WILL_HOOK, fn)
+        },
+        done: function(onSuccess, onFailure) {
+          return returns.hook(SUCCESS_HOOK, onSuccess).hook(FAILURE_HOOK, onFailure)
+        }
+      }
+    }
+
     return function (options, enqueue) {
       if (typeof options === 'function') {
         var _options = enqueue
@@ -30,17 +94,20 @@
       options = utils.normalizeOptions(options)
       var latestTimer, running = false, buffer = [], enqueuedCount = 0
 
-      var syncEnqueue = function (args) {
-        if (utils.invoke(options.will, args) === false) {
-          return utils.invoke(options.rejected, args)
+      var syncEnqueue = function (data) {
+        var ret, args
+        try {
+          args = utils.invoke(data.willHooks, data.args, true)
+          ret = utils.invoke(enqueue, args)
+        } catch (e) {
+          return failure(data, e)
         }
         try {
-          utils.invoke(enqueue, args)
           dequeueWithTimes(++enqueuedCount - options.limit)
         } catch (e) {
-          throw e
+          utils.warn(e)
         }
-        utils.invoke(options.did, args)
+        success(data, ret)
       }
 
       var dequeueWithTimes = function (times) {
@@ -52,10 +119,10 @@
         }
       }
 
-      var asyncEnqueue = function (args, delay, cb) {
+      var asyncEnqueue = function (data, delay, cb) {
         var timer = setTimeout(function () {
           clearTimeout(timer)
-          syncEnqueue(args)
+          syncEnqueue(data)
           utils.invoke(cb)
         }, delay)
       }
@@ -66,35 +133,44 @@
           return
         }
         while (overflowLen--) {
-          utils.invoke(options.rejected, buffer.shift())
+          failure(buffer.shift(), OVERFLOW_ERROR)
         }
       }
 
       var next = function () {
-        var args = buffer.shift()
-        if (typeof args === 'undefined') {
+        var data = buffer.shift()
+        if (typeof data === 'undefined') {
           return running = false
         }
-        var delay = utils.generateDelay(options.delay, args)
-        if (typeof delay === 'number' && delay < 0) {
-          syncEnqueue(args)
-          return next()
-        }
+        var delay = utils.computeDelay(data.delay, data.args, 0)
         if (utils.isPromise(delay)) {
-          return delay.then(function () {
-            syncEnqueue(args)
+          return delay.then(function (res) {
+            if (typeof res !== 'undefined') {
+              data.args = [res]
+            }
+            syncEnqueue(data)
+            next()
+          }, function (e) {
+            failure(data, e)
             next()
           })
         }
-        asyncEnqueue(args, delay, function () {
-          next()
-        })
+        asyncEnqueue(data, delay, next)
       }
 
       return function () {
-        var args = Array.prototype.slice.call(arguments)
-        buffer.push(args)
-        var timer = latestTimer = setTimeout(function () {
+        var timer, data = {
+          args: Array.prototype.slice.call(arguments),
+          delay: options.delay,
+          willHooks: typeof options[WILL_HOOK] === 'function' ? [options[WILL_HOOK]] : [],
+          successHooks: typeof options[SUCCESS_HOOK] === 'function' ? [options[SUCCESS_HOOK]] : [],
+          failureHooks: typeof options[FAILURE_HOOK] === 'function' ? [options[FAILURE_HOOK]] : [],
+          status: PENDING,
+          value: void 0,
+          error: void 0
+        }
+        buffer.push(data)
+        timer = latestTimer = setTimeout(function () {
           clearTimeout(timer)
           if (timer !== latestTimer) {
             return
@@ -105,61 +181,72 @@
             next()
           }
         }, 0)
+
+        return createLazyReturns(data)
       }
     }
   }
 }(function () {
-  var noop = function () {}
+  var warn = function (e) {
+    if (console) {
+      return console.warn(e)
+    }
+    throw e
+  }
 
   var isValidNumber = function (number) {
     return typeof number === 'number' && !isNaN(number)
-  }
-
-  var invoke = function (fn, args, content) {
-    if (typeof fn === 'function') {
-      return fn.apply(content || null, args || [])
-    }
   }
 
   var isPromise = function (target) {
     return target && typeof target.then === 'function'
   }
 
-  var generateDelay = function (delay, args) {
+  var isValidDelay = function (delay) {
+    return isValidNumber(delay) || isPromise(delay)
+  }
+
+  var computeDelay = function (delay, args, $default) {
     if (typeof delay === 'function') {
       delay = invoke(delay, args)
     }
+    return isValidDelay(delay) ? delay : $default
+  }
 
-    return (isValidNumber(delay) || isPromise(delay)) ? delay : 0
+  var invoke = function (fn, args, isCrossArgs) {
+    var ret, len
+    if (fn && (len = fn.length)) {
+      while (len--) {
+        ret = invoke(fn[len], args)
+        if (isCrossArgs && typeof ret !== 'undefined') {
+          args = [ret]
+        }
+        if (isCrossArgs && !len) return args
+      }
+    }
+    if (typeof fn === 'function') {
+      return fn.apply(null, args || [])
+    }
   }
 
   var normalizeOptions = function (options) {
-    if (!options || typeof options !== 'object') {
-      options = {}
-    }
-    options.delay = options.delay || 0
-    options.will = options.will || noop
-    options.did = options.did || noop
-    options.rejected = options.rejected || noop
+    options = options || {}
     options.limit = isValidNumber(options.limit) ? options.limit : Infinity
     options.dequeue = options.dequeue || void 0
-    if (!isValidNumber(options.delay) && typeof options.delay !== 'function') {
-      throw new Error('Excepted the delay option to be a number or function.')
-    }
     if (options.limit < 0) {
       throw new Error('Excepted the limit option greater than or equal to zero.')
     }
     if (options.limit < Infinity && typeof options.dequeue !== 'function') {
       throw new Error('Excepted the dequeue option to be a function.')
     }
-
     return options
   }
 
   return {
+    warn: warn,
     invoke: invoke,
     isPromise: isPromise,
-    generateDelay: generateDelay,
+    computeDelay: computeDelay,
     normalizeOptions: normalizeOptions
   }
 }())))
